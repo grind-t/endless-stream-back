a3d3072c23dead05434d2cb9d6bae1e4712067d7
grind-t <grindt.gd@gmail.com>
2021-12-05 01:31:27 +0300

    feat: media

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,63 @@
 import { start } from "./server.js";
 
-const { twitch } = await start();
+interface MediaRequest {
+  user: string;
+  videoId: string;
+}
+
+interface Media {
+  queue: MediaRequest[];
+  maxQueue: number;
+  current: MediaRequest | undefined;
+}
+
+const media: Media = {
+  queue: [],
+  maxQueue: 100,
+  current: undefined,
+};
+
+const { twitch, youtube } = await start();
 twitch.chat.say(twitch.channel, "чупапи муняня");
+
+async function handleMessage(
+  user: string,
+  message: string,
+  say: (message: string) => Promise<void>
+) {
+  if (message[0] != "!") return;
+  const [command, arg] = message.split(" ", 2);
+  switch (command) {
+    case "!плейлист+": {
+      if (media.queue.length >= media.maxQueue) {
+        const errorMessage = `@${user}, плейлист переполнен 🤕`;
+        say(errorMessage);
+        return;
+      }
+      const videoId = arg.match(/(.*?)(^|\/|v=)([a-z0-9_-]{11})(.*)?/i)?.at(3);
+      if (!videoId) {
+        const errorMessage = `@${user}, непонятная ссылка 🤕`;
+        say(errorMessage);
+        return;
+      }
+      const video = await youtube.videos
+        .list({ id: [videoId], part: ["snippet", "statistics"] })
+        .then((r) => r.data.items?.at(0))
+        .catch(console.error);
+      if (!video) {
+        const errorMessage = `@${user}, видео не найдено 🤕`;
+        say(errorMessage);
+        return;
+      }
+      media.queue.push({ user, videoId });
+      if (!media.current) media.current = media.queue.shift();
+      const successMessage = `@${user} добавил в плейлист "${video.snippet?.title}"`;
+      say(successMessage);
+      break;
+    }
+  }
+}
+
+twitch.chat.onMessage((channel, user, message) =>
+  handleMessage(user, message, (message) => twitch.chat.say(channel, message))
+);
