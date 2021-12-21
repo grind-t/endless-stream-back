a8c91eb686a9440653f0fa306ecea02fdf8edbb8
grind-t <grindt.gd@gmail.com>
2021-12-21 22:33:47 +0300

    feat: show media on connection

--- src/events/socket.ts
+++ src/events/socket.ts
@@ -1,6 +1,10 @@
 import { Socket } from 'socket.io'
+import { getIO } from '../server.js'
 import { handleMediaEnd } from './media.js'
+import { media } from '../data/media.js'
 
-export function handleConnection(socket: Socket) {
+export function handleConnection(socket: Socket): void {
   socket.on('media/ended', handleMediaEnd)
+  if (media.current) getIO().emit('media/changed', media.current)
+  else handleMediaEnd()
 }
