2932797407e0146f866cc5658433da8a07db1cd9
grind-t <grindt.gd@gmail.com>
2021-12-27 22:18:15 +0300

    feat: poll class

---
+++ src/lib/poll.ts
@@ -0,0 +1,33 @@
+import { User } from '../clients/app.js'
+
+export interface Vote {
+  user: User
+  choice: string
+}
+
+export class Poll {
+  private choices: string[]
+  private votes: Map<string, Vote>
+  private totalVotes: Record<string, number>
+  private leader: string
+
+  constructor(choices: string[]) {
+    this.choices = choices
+    this.votes = new Map()
+    this.totalVotes = Object.fromEntries(choices.map((v) => [v, 0]))
+    this.leader = choices[0]
+  }
+
+  getLeader(): string {
+    return this.leader
+  }
+
+  vote(user: User, choice: string) {
+    const prevVote = this.votes.get(user.id)
+    if (prevVote) this.totalVotes[prevVote.choice]--
+    this.votes.set(user.id, { user, choice })
+    const choiceVotes = ++this.totalVotes[choice]
+    const leaderVotes = this.totalVotes[this.leader]
+    if (choiceVotes > leaderVotes) this.leader = choice
+  }
+}
